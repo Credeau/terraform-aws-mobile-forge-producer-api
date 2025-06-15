250a4dfb90480dd3a55024334d9729572f624e8c
Harshit Sharma <brown.hash@outlook.com>
2025-06-15 13:26:02 +0530

    add awf required prefix in log group name

--- logs.tf
+++ logs.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_log_group" "main" {
 }
 
 resource "aws_cloudwatch_log_group" "waf" {
-  name              = format("%s-waf-logs", local.stack_identifier)
+  name              = format("aws-waf-logs-%s", local.stack_identifier) # waf needs this prefix before log group names `aws-waf-logs-`
   retention_in_days = var.logs_retention_period
   tags              = local.common_tags
 }
